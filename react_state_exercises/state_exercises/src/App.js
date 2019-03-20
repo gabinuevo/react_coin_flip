fe22260ee2930e5244302455342758762c54e114
Sandy <sangzicao@gmail.com>
2019-03-19 17:18:37 -0700

    complete megabox component

--- react_state_exercises/state_exercises/src/App.js
+++ react_state_exercises/state_exercises/src/App.js
@@ -6,8 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <Megabox/>
-          <button>OBAMA 2020</button>
+          <Megabox />
       </div>
     );
   }
--- react_state_exercises/state_exercises/src/MegaBox.js
+++ react_state_exercises/state_exercises/src/MegaBox.js
@@ -6,12 +6,32 @@ class MegaBox extends Component {
         colors: ['peachpuff', 'tomato', 'BurlyWood', 'OliveDrab', 'PapayaWhip', 'RebeccaPurple', 'Peru', 'LightSalmon', 'Indigo',
         'LawnGreen', 'LightSalmon', 'Maroon', 'MidnightBlue', 'Orchid', 'Plum', 'SandyBrown']
     }
+
+    constructor(props) {
+        super(props);
+        this.changeRandoBox = this.changeRandoBox.bind(this);
+        this.state = {colors: this.props.colors.slice()};
+    }
+
+
+    changeRandoBox(evt) {
+        const randIdx = Math.floor(Math.random() * this.state.colors.length);
+        const randColor = this.props.colors[Math.floor(Math.random() * this.state.colors.length)];
+        const newColors = this.state.colors.slice();
+        newColors[randIdx] = randColor;
+        console.log("THIS IS WHAT NEWCOLORS IS:", newColors)
+        console.log("THIS IS WHAT RANDIDX IS:", randIdx)
+        console.log("THIS IS WHAT RANDCOLOR IS:", randColor)
+        this.setState({colors: newColors});
+    }
+
     render() {
-        const boxes = this.props.colors
-                .map(color => (<Box color={color}/>))
+        const boxes = this.state.colors
+                .map(color => (<Box color={color}/>));
         return (
         <div className="MegaBox">
             {boxes}
+            <button onClick={this.changeRandoBox}>OBAMA 2020</button>
         </div>
         );
     }
